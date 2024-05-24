552aff21e3b12935c928ad3e69e4fd557b061bab
martinrios28 <martin.rioset7@gmail.com>
2024-05-24 15:12:29 -0300

    commitjaja

--- tp3.java
+++ tp3.java
@@ -8,14 +8,7 @@ class tp3
         Vector<String> alumnos= new Vector<>();
 
         //TODO Agrega una línea con tu nombre y apellido
-        alumnos.add("Elian Guzman");
-        alumnos.add("Alejandro villar");
-        alumnos.add("Luz Arancibia");
-        alumnos.add("Federico Lafflitto");
-        alumnos.add("Thiago Eberbach");
-        alumnos.add("Mia Valentina Inzunza");
-        alumnos.add("Santiago Beron");
-        alumnos.add("Thomas Fazio");
+        alumnos.add("Martin Rios");
 
       System.out.println("Alumnos " + alumnos);        
     }
