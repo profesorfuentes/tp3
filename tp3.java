a42e43a007cc830cffd00cffa60f5123393b387c
profesorfuentes <jose.fuentes@bue.edu.ar>
2024-05-24 12:19:57 -0300

    Add files via upload

---
+++ tp3.java
@@ -0,0 +1,15 @@
+import java.util.Vector;
+
+class tp3
+{
+    public static void main(String []args)
+    {
+
+        Vector<String> alumnos= new Vector<>();
+
+        //TODO Agrega tu nombre
+        alumnos.add("Nombre Apellido");
+
+        System.out.println("Alumnos " + alumnos);        
+    }
+};
