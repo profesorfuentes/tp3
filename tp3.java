dbd56f77854efe932abd64697ec724558465a4e6
Eberbachh <135264054+Eberbachh@users.noreply.github.com>
2024-05-24 13:54:22 -0300

    Update tp3.java

--- tp3.java
+++ tp3.java
@@ -8,7 +8,7 @@ class tp3
         Vector<String> alumnos= new Vector<>();
 
         //TODO Agrega una línea con tu nombre y apellido
-        alumnos.add("Nombre Apellido");
+        alumnos.add("Thiago Eberbach");
 
         System.out.println("Alumnos " + alumnos);        
     }
