fd75c11db2e5d49616e6f7ed8615b1fcaa10e076
Anchoas06 <135259849+Anchoas06@users.noreply.github.com>
2024-05-24 14:28:24 -0300

    Update tp3.java

--- tp3.java
+++ tp3.java
@@ -8,7 +8,6 @@ class tp3
         Vector<String> alumnos= new Vector<>();
 
         //TODO Agrega una línea con tu nombre y apellido
-        alumnos.add("Nombre Apellido");
         alumnos.add("Luz Arancibia");
 
         System.out.println("Alumnos " + alumnos);        
